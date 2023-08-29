bed64839b0d9225c10b41146d23662f05f0ad23d
mikeborodin <streamofbee@gmail.com>
2023-08-29 09:44:14 +0200

    [add] nvim surround

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -46,6 +46,16 @@ return {
     config = function()
       require("vstask").setup()
     end
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
   }
 
 }
