8eb7af57941f2e95e3652c64a4330209288fc917
mikeborodin <streamofbee@gmail.com>
2024-01-07 18:22:12 +0100

    [update] nvim, tmux

--- tmux/status.sh
+++ tmux/status.sh
@@ -1,3 +1,6 @@
 set -e
 
-echo "T:$(date +'%I:%M')"
+cd /Users/mike/personal_projects/piper
+
+dart run  --define=AZURE_TOKEN=$AZURE_TOKEN  cli/bin/piper_cli.dart watch
+# echo "$pipelines T:$(date +'%I:%M')"
