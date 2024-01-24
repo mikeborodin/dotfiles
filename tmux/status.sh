e3acc3d3f0377f7efb6b2b16a73c81f47e1f451e
mikeborodin <streamofbee@gmail.com>
2024-01-24 21:51:53 +0100

    update kitty conf, check in snippets

--- tmux/status.sh
+++ tmux/status.sh
@@ -2,5 +2,5 @@ set -e
 
 cd /Users/mike/personal_projects/piper
 
-dart run  --define=AZURE_TOKEN=$AZURE_TOKEN  cli/bin/piper_cli.dart watch
+dart run --define=AZURE_TOKEN=$AZURE_TOKEN cli/bin/piper_cli.dart read -f local/use_config.yaml -l line
 # echo "$pipelines T:$(date +'%I:%M')"
