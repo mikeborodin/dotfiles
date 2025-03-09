11da8b7e063da4f17daf1292f14b069619821567
mikeborodin <mikeborodin@users.noreply.github.com>
2025-03-09 22:46:15 +0100

    update toggleterm

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -4,6 +4,13 @@ return {
         require('toggleterm').setup({
             open_mapping = "<C-h>",
             direction = "float",
+            shell = function()
+                if vim.env.DEVBOX_PROJECT_ROOT == vim.loop.cwd() then
+                    return 'devbox run shell'
+                else
+                    return 'zsh'
+                end
+            end
         })
     end
 }
